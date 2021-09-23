a28acb4e1a6eea5d79b05ec9a41de901e59c188f
“chaihuo” <bravetsia@gmail.com>
2021-09-22 23:37:33 -0500

    add custom configuration items for Spring Boot

--- src/main/java/com/chai/wiki/controller/TestController.java
+++ src/main/java/com/chai/wiki/controller/TestController.java
@@ -1,15 +1,18 @@
 package com.chai.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:Default}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
