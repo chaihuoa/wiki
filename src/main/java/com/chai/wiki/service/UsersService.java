8cff272dafa02d8f7b75a38f1890aa8a92f0ce4c
“chaihuo” <bravetsia@gmail.com>
2021-12-09 20:49:01 -0600

    Encapsulating BeanUtils using CopyUtil

--- src/main/java/com/chai/wiki/service/UsersService.java
+++ src/main/java/com/chai/wiki/service/UsersService.java
@@ -5,11 +5,10 @@ import com.chai.wiki.domain.UsersExample;
 import com.chai.wiki.mapper.UsersMapper;
 import com.chai.wiki.req.UsersReq;
 import com.chai.wiki.resp.UsersResp;
-import org.springframework.beans.BeanUtils;
+import com.chai.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,12 +23,14 @@ public class UsersService {
         criteria.andUsernameLike("%" + req.getUsername() + "%");
         List<Users> usersList = UsersMapper.selectByExample(usersExample);
 
-        List<UsersResp> respList = new ArrayList<>();
-        for (Users users : usersList) {
-            UsersResp usersResp = new UsersResp();
-            BeanUtils.copyProperties(users, usersResp);
-            respList.add(usersResp);
-        }
+//        List<UsersResp> respList = new ArrayList<>();
+//        for (Users users : usersList) {
+////            UsersResp usersResp = new UsersResp();
+////            BeanUtils.copyProperties(users, usersResp);
+//            UsersResp usersResp = CopyUtil.copy(users, UsersResp.class);
+//            respList.add(usersResp);
+//        }
+        List<UsersResp> respList = CopyUtil.copyList(usersList, UsersResp.class);
         return respList;
     }
 }
