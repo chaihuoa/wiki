9d973ebf27ba2fef212efdfdbc69a989837ff497
“chaihuo” <bravetsia@gmail.com>
2021-12-18 14:51:33 -0600

    added LogAspect

--- src/main/java/com/chai/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/chai/wiki/config/SpringMvcConfig.java
@@ -1,23 +1,23 @@
-package com.chai.wiki.config;
-
-import com.chai.wiki.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LoginInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/login"
-                );
-    }
-}
+//package com.chai.wiki.config;
+//
+//import com.chai.wiki.interceptor.LoginInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LoginInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/login"
+//                );
+//    }
+//}
