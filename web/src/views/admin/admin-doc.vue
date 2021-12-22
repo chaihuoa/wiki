31c1f04e0c27a18c9ce655fac1ed2fd35ef9ae47
“chaihuo” <bravetsia@gmail.com>
2021-12-22 16:21:18 -0600

    Added a-tree-select element for parent form of a-modal.

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -62,7 +62,7 @@
 
   <a-modal
       v-model:visible="modalVisible"
-      title="分类表单"
+      title="文档表单"
       :confirm-loading="modalLoading"
       @ok="handleModalOk"
   >
@@ -72,17 +72,16 @@
         <a-input v-model:value="doc.name" />
       </a-form-item>
       <a-form-item label="父分类">
-        <a-select
-            v-model:value="doc.parent"
-            ref="select"
+        <a-tree-select
+          v-model:value="doc.parent"
+          style="width: 100%"
+          :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+          :tree-data="treeSelectData"
+          placeholder="请选择父文档"
+          tree-default-expand-all
+          :fieldNames="{children:'children', label:'name', key:'id', value: 'id' }"
         >
-          <a-select-option :value="0">
-            无
-          </a-select-option>
-          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="doc.id === c.id">
-            {{c.name}}
-          </a-select-option>
-        </a-select>
+        </a-tree-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
@@ -112,11 +111,6 @@ export default defineComponent({
         dataIndex: 'name',
         key: 'name'
       },
-      // {
-      //   title: '父分类',
-      //   key: 'parent',
-      //   dataIndex: 'parent'
-      // },
       {
         title: '顺序',
         key: 'sort',
@@ -152,6 +146,11 @@ export default defineComponent({
 
           level1.value = [];
           level1.value = Tool.array2Tree(docs.value, 0);
+
+          // 父文档下拉框初始化，相当于点击新增
+          treeSelectData.value = Tool.copy(level1.value) || [];
+          // 为选择树添加一个"无"
+          treeSelectData.value.unshift({id: 0, name: '无'});
         } else {
           message.error(data.message);
         }
@@ -159,6 +158,8 @@ export default defineComponent({
     };
 
     // -------- Form ---------
+    const treeSelectData = ref();
+    treeSelectData.value = [];
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
@@ -178,14 +179,58 @@ export default defineComponent({
       });
     };
 
+    /**
+     * 将某节点及其子孙节点全部置为disabled
+     */
+    const setDisable = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标节点设置为disabled
+          node.disabled = true;
+
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            setDisable(children, id);
+          }
+        }
+      }
+    };
+
     const edit = (record: any) => {
       modalVisible.value = true;
       doc.value = Tool.copy(record);
+
+      // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value, record.id);
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     const save = () => {
       modalVisible.value = true;
       doc.value = {};
+
+      // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     const handleDelete = (id: string) => {
@@ -220,7 +265,8 @@ export default defineComponent({
       doc,
       handleModalOk,
 
-      formState
+      formState,
+      treeSelectData
     }
   }
 });
