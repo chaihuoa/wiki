a77432886a33a1d85bae3249d6010bd5c2c3002b
“chaihuo” <bravetsia@gmail.com>
2021-12-26 13:24:07 -0600

    Created user table.

--- doc/all.sql
+++ doc/all.sql
@@ -87,3 +87,13 @@ create table `content` (
     content mediumtext not null,
     primary key (id)
 )
+
+drop table if exists `user`;
+create table `user` (
+    id bigint not null,
+    login_name varchar(50) not null,
+    name varchar(50),
+    password char(32) not null,
+    primary key (id),
+    unique key login_name_unique (login_name)
+)
