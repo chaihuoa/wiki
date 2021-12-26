b6c246e816ca0e7056bc94b39f8b3f36be777f40
“chaihuo” <bravetsia@gmail.com>
2021-12-26 15:30:11 -0600

    Prevents users from changing their user names.

--- src/main/java/com/chai/wiki/service/UserService.java
+++ src/main/java/com/chai/wiki/service/UserService.java
@@ -2,11 +2,13 @@ package com.chai.wiki.service;
 
 import com.chai.wiki.domain.User;
 import com.chai.wiki.domain.UserExample;
+import com.chai.wiki.exception.BusinessException;
+import com.chai.wiki.exception.BusinessExceptionCode;
 import com.chai.wiki.mapper.UserMapper;
 import com.chai.wiki.req.UserQueryReq;
 import com.chai.wiki.req.UserSaveReq;
-import com.chai.wiki.resp.UserQueryResp;
 import com.chai.wiki.resp.PageResp;
+import com.chai.wiki.resp.UserQueryResp;
 import com.chai.wiki.util.CopyUtil;
 import com.chai.wiki.util.SnowFlake;
 import com.github.pagehelper.PageHelper;
@@ -14,6 +16,7 @@ import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -62,14 +65,32 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(user.getId())) {
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if (ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                // 用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         } else {
-            userMapper.updateByPrimaryKey(user);
+            user.setLoginName(null);
+            user.setPassword(null);
+            userMapper.updateByPrimaryKeySelective(user);
         }
     }
 
     public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(LoginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+
+        return CollectionUtils.isEmpty(userList) ? null : userList.get(0);
     }
 }
