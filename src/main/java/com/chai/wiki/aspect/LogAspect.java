0af8f59b85ad0d8b8dc5ca867b72eec6b06f26f9
“chaihuo” <bravetsia@gmail.com>
2021-12-27 15:48:47 -0600

    Added the log flow number.

--- src/main/java/com/chai/wiki/aspect/LogAspect.java
+++ src/main/java/com/chai/wiki/aspect/LogAspect.java
@@ -3,6 +3,7 @@ package com.chai.wiki.aspect;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 import com.chai.wiki.util.RequestContext;
+import com.chai.wiki.util.SnowFlake;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
@@ -12,11 +13,13 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +34,9 @@ public class LogAspect {
     @Pointcut("execution(public * com.chai.*.controller..*Controller.*(..))")
     public void controllerPointcut() {}
 
+    @Resource
+    private SnowFlake snowFlake;
+
     @Before("controllerPointcut()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
 
@@ -39,6 +45,9 @@ public class LogAspect {
         HttpServletRequest request = attributes.getRequest();
         Signature signature = joinPoint.getSignature();
         String name = signature.getName();
+
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
 
         // 打印请求信息
         LOG.info("------------- 开始 -------------");
